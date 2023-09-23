ace2b05145fa8012ac93308b0c323d808dc875d6
mo7ammed3wad <m7mmd.3wad1927@gmail.com>
2023-09-22 17:26:37 -0700

    0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * function that concatenates two strings.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int v;
+	int c;
+
+	v = 0;
+	while (dest[v] != '\0')
+	{
+		v++;
+	}
+	c = 0;
+	while (src[c] != '\0')
+	{
+		dest[v] = src[c];
+		v++;
+		c++;
+	}
+	dest[v] = '\0';
+	return (dest);
+}
