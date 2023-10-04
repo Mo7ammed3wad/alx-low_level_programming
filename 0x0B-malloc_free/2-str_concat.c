718d3b2929e834124fc526c0af023ff848603018
mo7ammed3wad <m7mmd.3wad1927@gmail.com>
2023-10-04 16:33:21 -0700

    22

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,40 +2,42 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+
 /**
  * str_concat.
  */
 
 char *str_concat(char *s1, char *s2)
 {
-	char *s3;
-	unsigned int m = 0,	n = 0, ln1 = 0, ln2 = 0;
+	int v, k, ln1, ln2, ln;
+	char *result;
 
-	while (s1 && s1[ln1])
-		ln1++;
-	while (s2 && s2[ln2])
-		ln2++;
+	ln1 = ln2 = 0;
 
-	s3 = malloc(sizeof(char) * (ln1 + ln2 + 1));
-	if (s3 == NULL)
+	if (s1 != NULL)
+	{
+		v = 0;
+		while (s1[v++] != '\0')
+			ln1++;
+	}
+
+	if (s2 != NULL)
+	{
+		v = 0;
+		while (s2[v++] != '\0')
+			ln2++;
+	}
+
+	ln = ln1 + ln2;
+	result = (char *)malloc(sizeof(char) * (ln + 1));
+	if (result == NULL)
 		return (NULL);
 
-	if (s1)
-	{
-		for (m = 0; m < ln1; m++)
-			s3[m] = s1[m];
-	}
+	for (v = 0; v < ln1; v++)
+		result[v] = s1[v];
+	for (k = 0; k < ln2; k++, v++)
+		result[v] = s2[k];
+	result[ln] = '\0';
 
-	if (s2)
-	{
-		while (m < (ln1 + ln2))
-		{
-			s3[m] = s2[n];
-			m++;
-			n++;
-		}
-	}
-
-	s3[i] = '\0';
-	return (s3);
+	return (result);
 }
