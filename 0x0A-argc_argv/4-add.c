4f66436984525c569e66a552d4a414f728f802fd
mo7ammed3wad <m7mmd.3wad1927@gmail.com>
2023-10-07 15:00:26 -0700

    100

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,35 +1,74 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
-/**
- * add.
- */
-
-int main(int argc, char **argv)
+int _atoi(char *s)
 {
-	int x, o, sum = 0;
-	char *flag;
+	int v, x, n, len, g, digit;
 
-	if (argc < 2)
+	v = 0;
+	x = 0;
+	n = 0;
+	len = 0;
+	g = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (v < len && g == 0)
 	{
-		printf("0\n");
+		if (s[v] == '-')
+			++x;
+
+		if (s[v] >= '0' && s[v] <= '9')
+		{
+			digit = s[v] - '0';
+			if (x % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[v + 1] < '0' || s[v + 1] > '9')
+				break;
+			g = 0;
+		}
+		v++;
+	}
+
+	if (g == 0)
 		return (0);
-	}
 
-	for (x = 1; argv[x]; x++)
+	return (n);
+}
+
+
+int main(int argc, char *argv[])
+{
+	int sum, n, a, j, k;
+
+	sum = 0;
+
+	for (a = 1; a < argc; a++)
 	{
-		n = strtol(argv[x], &flag, 10);
-		if (*flag)
+		for (j = 0; argv[a][j] != '\0'; j++)
 		{
-			printf("Error\n");
-			return (1);
-		}
-		else
-		{
-			sum += o;
+			if (argv[a][j] > '9' || argv[a][j] < '0')
+			{
+				puts("Error");
+				return (1);
+			}
 		}
 	}
-	printf("%d\n", sum);
 
+	for (k = 1; k < argc; k++)
+	{
+		n = _atoi(argv[k]);
+		if (n >= 0)
+		{
+			sum += n;
+		}
+	}
+
+	printf("%d\n", sum);
 	return (0);
 }
